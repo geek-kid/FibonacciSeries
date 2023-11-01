164bbe6bca423351df47d3463d2344a2e2fa14b7
Arya <aryatahery@gmail.com>
2023-11-01 08:16:41 +0330

    Add project files.

---
+++ FibonacciSeries/Program.cs
@@ -0,0 +1,18 @@
+﻿int firstNum = 0, secodnNum = 1, result = 0, n;
+Console.WriteLine("n?");
+try
+{
+    n = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Bad Input Try Again");
+    return;
+}
+for (int i = 0; i <= n; i++)
+{
+    result = firstNum + secodnNum;
+    firstNum = secodnNum;
+    secodnNum = result;
+}
+Console.WriteLine(result);
